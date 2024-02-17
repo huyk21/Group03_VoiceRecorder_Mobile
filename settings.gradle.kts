189585276b92256105f36a48c821bcc7ce92d9ef
huyk21 <92777449+huyk21@users.noreply.github.com>
2024-02-17 14:44:37 +0700

    Initial commit

---
+++ settings.gradle.kts
@@ -0,0 +1,18 @@
+pluginManagement {
+    repositories {
+        google()
+        mavenCentral()
+        gradlePluginPortal()
+    }
+}
+dependencyResolutionManagement {
+    repositoriesMode.set(RepositoriesMode.FAIL_ON_PROJECT_REPOS)
+    repositories {
+        google()
+        mavenCentral()
+    }
+}
+
+rootProject.name = "Group03_VoiceRecorder_Mobile"
+include(":app")
+ 
