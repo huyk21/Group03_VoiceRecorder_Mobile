8ad7ba09a10dd09621ae80f6be95e0df242ae87f
unknown <phamquocduy3112003@gmail.com>
2024-03-30 17:01:28 +0700

    added modifications

---
+++ app/src/main/java/com/example/group03_voicerecorder_mobile/audio/recorder/RecorderInterface.java
@@ -0,0 +1,25 @@
+package com.example.group03_voicerecorder_mobile.audio.recorder;
+
+import com.example.group03_voicerecorder_mobile.exception.ApplicationException;
+
+import java.io.File;
+
+public interface RecorderInterface {
+    interface RecorderCallback {
+        void onStart(File outputFile);
+        void onPause();
+        void onResume();
+        void onRecording();
+        void onStop(File outputFile);
+        void onError(ApplicationException exc);
+    }
+
+    interface Recorder {
+        void setCallBack(RecorderCallback callBack);
+        void startRecord(String output, int channel, int bitrate, int samplerate);
+        void pauseRecord();
+        void resumeRecord();
+        void stopRecord();
+        boolean isRecording();
+    }
+}
