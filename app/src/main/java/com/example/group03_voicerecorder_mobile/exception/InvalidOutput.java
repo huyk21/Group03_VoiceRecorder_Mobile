69327c458393a0407b20e2a172ec04d07f1be25c
unknown <phamquocduy3112003@gmail.com>
2024-03-30 17:02:42 +0700

    added exceptions

--- app/src/main/java/com/example/group03_voicerecorder_mobile/exception/InvalidOutput.java
+++ app/src/main/java/com/example/group03_voicerecorder_mobile/exception/InvalidOutput.java
@@ -1,2 +1,8 @@
-package com.example.group03_voicerecorder_mobile.exception;public class InvalidOutput {
+package com.example.group03_voicerecorder_mobile.exception;
+
+public class InvalidOutput extends ApplicationException{
+    @Override
+    public int getExcepType() {
+        return ApplicationException.INVALID_OUTPUT_EXCEPTION;
+    }
 }
--- app/src/main/java/com/example/group03_voicerecorder_mobile/exception/ReadPermissionError.java
+++ app/src/main/java/com/example/group03_voicerecorder_mobile/exception/ReadPermissionError.java
@@ -1,2 +1,8 @@
-package com.example.group03_voicerecorder_mobile.exception;public class ReadPermissionError {
+package com.example.group03_voicerecorder_mobile.exception;
+
+public class ReadPermissionError extends ApplicationException{
+    @Override
+    public int getExcepType() {
+        return ApplicationException.PERMISSION_READ_EXCEPTION;
+    }
 }
