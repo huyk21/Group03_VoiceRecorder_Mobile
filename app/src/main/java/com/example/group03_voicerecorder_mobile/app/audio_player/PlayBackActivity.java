e5c612fb3970e5c5151e9d9dfcef4cff410b7ead
huyk21 <92777449+huyk21@users.noreply.github.com>
2024-04-11 10:58:02 +0700

    Edit record activity UI, add acitivty playback

---
+++ app/src/main/java/com/example/group03_voicerecorder_mobile/app/audio_player/PlayBackActivity.java
@@ -0,0 +1,17 @@
+package com.example.group03_voicerecorder_mobile.app.audio_player;
+
+import android.os.Bundle;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import com.example.group03_voicerecorder_mobile.R;
+
+public class PlayBackActivity extends AppCompatActivity {
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        // Set the content view to your XML layout for the playback activity
+        setContentView(R.layout.activity_playback);
+        // Your playback logic here
+    }
+}
