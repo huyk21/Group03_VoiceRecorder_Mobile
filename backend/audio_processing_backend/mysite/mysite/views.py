f3265cb0af85b49dbd63e686d0de554dcfc0ca59
QuyHoa <quyhoaphantruong@gmail.com>
2024-04-17 22:57:48 +0700

    restrict some config files in django

--- backend/audio_processing_backend/mysite/mysite/views.py
+++ backend/audio_processing_backend/mysite/mysite/views.py
@@ -2,5 +2,5 @@ from django.http import JsonResponse
 
 def get_names(request):
     # Logic to fetch or generate the list of names
-    names = ["Alice", "Bob", "Charlie", "David"]  # Replace with your logic
+    names = ["Alice", "Bob", "Charlie", "David"]  
     return JsonResponse({"names": names})
