c6864a4126e1407db5c1de1d45c13e47132b61eb
QuyHoa <quyhoaphantruong@gmail.com>
2024-04-19 17:48:04 +0700

    add dependencies for handling api

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -28,6 +28,7 @@ android {
     }
 }
 
+
 dependencies {
     implementation("androidx.appcompat:appcompat:1.6.1")
     implementation("com.google.android.material:material:1.11.0")
@@ -35,5 +36,15 @@ dependencies {
     testImplementation("junit:junit:4.13.2")
     androidTestImplementation("androidx.test.ext:junit:1.1.5")
     androidTestImplementation("androidx.test.espresso:espresso-core:3.5.1")
+
+    implementation ("com.squareup.retrofit2:retrofit:2.9.0")
+    implementation ("com.squareup.retrofit2:converter-gson:2.9.0")
+    implementation ("com.squareup.okhttp3:okhttp:4.9.0")
+    implementation ("com.squareup.okhttp3:logging-interceptor:4.9.0")
+
+    // Retrofit converter for sending multipart requests
+    implementation ("com.squareup.retrofit2:converter-scalars:2.9.0")
+    // Gson for JSON parsing
+    implementation ("com.google.code.gson:gson:2.8.9")
 
 }
