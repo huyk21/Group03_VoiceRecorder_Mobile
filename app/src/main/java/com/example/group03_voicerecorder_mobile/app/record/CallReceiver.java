57c2f1d1e53864d44afde214840544f3270a3dc5
huyk21 <92777449+huyk21@users.noreply.github.com>
2024-05-02 11:26:59 +0700

    switch to record when receive phone call

--- app/src/main/java/com/example/group03_voicerecorder_mobile/app/record/CallReceiver.java
+++ app/src/main/java/com/example/group03_voicerecorder_mobile/app/record/CallReceiver.java
@@ -15,11 +15,12 @@ public class CallReceiver extends BroadcastReceiver {
             String state = intent.getStringExtra(TelephonyManager.EXTRA_STATE);
             if (TelephonyManager.EXTRA_STATE_RINGING.equals(state)) {
                 Toast.makeText(context, "Ringing", Toast.LENGTH_SHORT).show();
-            } else if (TelephonyManager.EXTRA_STATE_OFFHOOK.equals(state)) {
-                Toast.makeText(context, "Off Hook", Toast.LENGTH_SHORT).show();
                 Intent startIntent = new Intent(context, RecordService.class);
                 startIntent.setAction("ACTION_START_RECORDING");
                 context.startService(startIntent);
+            } else if (TelephonyManager.EXTRA_STATE_OFFHOOK.equals(state)) {
+                Toast.makeText(context, "Off Hook", Toast.LENGTH_SHORT).show();
+
             } else if (TelephonyManager.EXTRA_STATE_IDLE.equals(state)) {
                 Toast.makeText(context, "Idle", Toast.LENGTH_SHORT).show();
                 Intent stopIntent = new Intent(context, RecordService.class);
